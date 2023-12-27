58f0d4e133734d4610f3b93c045bc6fe82122a61
Jordan Carr <18126756+jordancarr@users.noreply.github.com>
2023-12-27 21:47:53 +0000

    Implement Fibonacci sequence via recursive function with cached results
    Signed-off-by: Jordan Carr <18126756+JordanCarr@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let verbose = true;
+
+    // Fibonacci sequence
+    let fib_num = 64usize;
+    println!("Print first {} Fibonacci numbers", fib_num);
+    fib_runner(fib_num, verbose);
+}
+
+fn fib_runner(number: usize, verbose: bool) {
+    let mut computed = Vec::<usize>::new();
+    for n in 0..=number {
+        let result = fib(n, &mut computed);
+        computed.push(result);
+    }
+
+    if verbose == true {
+        for n in 0..computed.len() {
+            println!("{}: {}", n, computed[n]);
+        }
+    } else if computed.last().is_some() {
+        println!("{}: {}", computed.len() - 1, computed.last().unwrap());
+    } else {
+        panic!("Error: None as last in Vector")
+    }
+}
+
+fn fib(number: usize, computed: &mut Vec<usize>) -> usize {
+    if computed.get(number).is_none() {
+        return match &number {
+            0 => 0,
+            1 => 1,
+            _ => fib(number - 1, computed) + fib(number - 2, computed),
+        };
+    }
+    return computed[number];
 }
